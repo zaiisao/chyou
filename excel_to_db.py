719ccbb87a05938e13c266d30efcf613b68ef5ca
Jaehoon Ahn <ahnza245@gmail.com>
2023-05-12 19:01:15 +0900

    Add data population commands

--- excel_to_db.py
+++ excel_to_db.py
@@ -16,14 +16,14 @@ def main():
 	df = pd.read_excel(file)
 	df = df.iloc[7:]
 
-	people = []
+	members = []
 
 	for row in df.iterrows():
 		name = row[1][2]
 		maeul = row[1][14] if row[1][14] != 0 else None
 		mokjang = row[1][15] if row[1][14] != 0 else None
 
-		people.append({
+		members.append({
 			'name': name,
 			'maeul': maeul,
 			'mokjang': mokjang
@@ -37,13 +37,20 @@ def main():
 	        user="juyoung32",
 	        password=password,
 	        host="chyou.co.kr",
-	        port=3306#,
-	        #database="employees"
-
+	        port=3306,
+	        database="juyoung32"
 	    )
 	except mariadb.Error as e:
 	    print(f"Error connecting to MariaDB Platform: {e}")
 	    sys.exit(1)
+
+	cur = conn.cursor()
+	cur.execute("CREATE TABLE if not exists members (name VARCHAR(255), maeul VARCHAR(255), mokjang VARCHAR(255))")
+	sql = "INSERT INTO members (name, maeul, mokjang) VALUES (%s,%s,%s)"
+
+	members_tuple = [tuple((member["name"], member["maeul"], member["mokjang"])) for member in members]
+	cur.executemany(sql, members_tuple)
+	conn.commit()
 
 if __name__ == '__main__':
     main()
